3687eee814353c5b62f3c1ca8005a3a5a26ffc5b
kudesnick <kudesnick@inbox.ru>
2020-10-18 13:02:40 +0300

    [fix] after tests

--- stage_usr/02-usb-console/01-run.sh
+++ stage_usr/02-usb-console/01-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
 # instead of "systemctl enable getty@ttyGS0.service" on ch_root mode
-ln -s /lib/systemd/system/getty@.service "${ROOTFS_DIR}/etc/systemd/system/getty.target.wants/getty@ttyGS0.service"
-
+SIM_LINK="${ROOTFS_DIR}/etc/systemd/system/getty.target.wants/getty@ttyGS0.service"
+if [ ! -L ${SIM_LINK} ] ; then
+	ln -s /lib/systemd/system/getty@.service ${SIM_LINK}
+fi
