763357a9603b6749ed4494ecdcdd212aff92d71a
Stulov Tikhon <stulov@safit.pro>
2024-08-29 19:12:52 +0300

    refactoring network tweak and fix gamepad connecting

--- stage2_usr/06-network-tweak/00-run.sh
+++ stage2_usr/06-network-tweak/00-run.sh
@@ -3,49 +3,25 @@
 # see https://github.com/raspberrypi/bookworm-feedback/issues/72#issuecomment-1848778447
 
 if [ $WPA_ESSID ] && [ $WPA_PASSWORD ]; then
-    WPA_LIST=("${WPA_ESSID}:${WPA_PASSWORD}")
+	WPA_LIST=("${WPA_ESSID}:${WPA_PASSWORD}")
 fi
 
 PRIORITY=0
 
 for WPA in "${WPA_LIST[@]}" ; do
+	ESSID="${WPA%%:*}"
+	PASSWORD="${WPA##*:}"
+	((PRIORITY++))
+	FILE_NAME="/etc/NetworkManager/system-connections/${ESSID}.nmconnection"
 
-ESSID="${WPA%%:*}"
-PASSWORD="${WPA##*:}"
-((PRIORITY++))
+	# nmcli dev wifi connect ${ESSID} password ${PASSWORD}
+	# nmcli connection modify ${ESSID} connection.autoconnect yes
+	# nmcli connection modify ${ESSID} connection.autoconnect-priority ${PRIORITY}
 
-# nmcli dev wifi connect ${ESSID} password ${PASSWORD}
-# nmcli connection modify ${ESSID} connection.autoconnect yes
-cat >${ROOTFS_DIR}/etc/NetworkManager/system-connections/${ESSID}.nmconnection <<EOF
-[connection]
-id=${ESSID}
-uuid=$(uuidgen)
-type=wifi
-interface-name=wlan0
-autoconnect=true
-autoconnect-priority=${PRIORITY}
-
-[wifi]
-mode=infrastructure
-ssid=${ESSID}
-
-[wifi-security]
-auth-alg=open
-key-mgmt=wpa-psk
-psk=$(wpa_passphrase ${ESSID} ${PASSWORD} | grep -E '[^#]psk=' | sed 's/.*psk=//')
-
-[ipv4]
-method=auto
-
-[ipv6]
-addr-gen-mode=default
-method=auto
-
-[proxy]
-EOF
-on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}"
-    chmod 600 /etc/NetworkManager/system-connections/${ESSID}.nmconnection
-EOF
-
+	install -m 600 files/wifi.connection ${ROOTFS_DIR}${FILE_NAME}
+	sed -i "s/id=/id=${ESSID}/"
+	sed -i "s/uuid=/uuid=$(uuidgen)/"
+	sed -i "s/autoconnect-priority=/autoconnect-priority=${PRIORITY}/"
+	sed -i "s/ssid=/ssid=${ESSID}/"
+	sed -i "s:psk=:psk=$(wpa_passphrase ${ESSID} ${PASSWORD} | grep -E '[^#]psk=' | sed 's/.*psk=//'):"
 done # WPA
--- stage2_usr/08-hid-sony/01-run.sh
+++ stage2_usr/08-hid-sony/01-run.sh
@@ -3,3 +3,5 @@
 install -m 755 files/ps3pair.sh     "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 install -m 755 files/ps4pair.sh     "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 install -m 755 files/show_charge.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+
+sed -i "s/#ClassicBondedOnly=true/ClassicBondedOnly=false/" "${ROOTFS_DIR}/etc/bluetooth/input.conf"
