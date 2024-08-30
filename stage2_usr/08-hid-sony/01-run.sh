8df5cc5db09573ce07781d2b983465f6540bfaff
Stulov Tikhon <stulov@safit.pro>
2024-08-30 15:25:10 +0300

    fix gamepad connecting

--- stage2_usr/08-hid-sony/01-run.sh
+++ stage2_usr/08-hid-sony/01-run.sh
@@ -4,4 +4,6 @@ install -m 755 files/ps3pair.sh     "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 install -m 755 files/ps4pair.sh     "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 install -m 755 files/show_charge.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 
+#see https://wiki.archlinux.org/title/Gamepad#PlayStation_3_controller
 sed -i "s/#ClassicBondedOnly=true/ClassicBondedOnly=false/" "${ROOTFS_DIR}/etc/bluetooth/input.conf"
+sed -i "s/#UserspaceHID=true/UserspaceHID=false/" "${ROOTFS_DIR}/etc/bluetooth/input.conf"
