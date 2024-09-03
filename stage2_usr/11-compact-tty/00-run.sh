26e8fb9af78c766db338b529c1655e4e930b3671
Stulov Tikhon <stulov@safit.pro>
2024-09-03 20:58:34 +0300

    drop banner

--- stage2_usr/11-compact-tty/00-run.sh
+++ stage2_usr/11-compact-tty/00-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
-patch --no-backup-if-mismatch -f "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.bashrc" < "files/.bashrc.diff"
+patch --no-backup-if-mismatch -f "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.bashrc" < "files/.bashrc.diff" || true
+rm -f "${ROOTFS_DIR}/etc/motd"
+sed -i "s/#PrintLastLog yes/PrintLastLog no/" "${ROOTFS_DIR}/etc/ssh/sshd_config"
