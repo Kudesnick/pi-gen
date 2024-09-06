b30c586b9e199696a6fd52ba205ae349c7d25a2e
Stulov Tikhon <stulov@safit.pro>
2024-09-06 17:37:25 +0300

    fix after testing

--- stage2_usr/00-bookworm/00-run.sh
+++ stage2_usr/00-bookworm/00-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-if [ "${RELEASE}" -eq "bookworm" ]; then
+if [ "${RELEASE}" == "bookworm" ]; then
     rm -fr ${ROOTFS_DIR}/boot/config.txt ${ROOTFS_DIR}/boot/cmdline.txt ${ROOTFS_DIR}/boot/overlays
-    ln -s firmware/config.txt ${ROOTFS_DIR}/boot/config.txt
-    ln -s firmware/cmdline.txt ${ROOTFS_DIR}/boot/cmdline.txt
-    ln -s firmware/overlays ${ROOTFS_DIR}/boot/overlays
-elif [ "${RELEASE}" -ne "bullseye" ]; then
+    ln -fs firmware/config.txt ${ROOTFS_DIR}/boot/config.txt
+    ln -fs firmware/cmdline.txt ${ROOTFS_DIR}/boot/cmdline.txt
+    ln -fs firmware/overlays ${ROOTFS_DIR}/boot/overlays
+elif [ "${RELEASE}" != "bullseye" ]; then
     echo "RELEASE must be bullseye or bookworm only"
     exit 1
 fi
