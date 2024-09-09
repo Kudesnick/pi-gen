47189fff7427789fcc9b705302484e16bbd2c73e
Stulov Tikhon <stulov@safit.pro>
2024-09-09 15:15:25 +0300

    fix after testing

--- stage2_usr/04-autologin-console/00-run.sh
+++ stage2_usr/04-autologin-console/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 if [ $USR_AUTOLOGIN ]; then
-    sed -i "s/-o '-p -- \\\*\\\u'/--autologin ${FIRST_USER_NAME}/" "${ROOTFS_DIR}/usr/lib/systemd/system/getty@.service"
-    sed -i "s/-o '-p -- \\\*\\\u'/--autologin ${FIRST_USER_NAME}/" "${ROOTFS_DIR}/usr/lib/systemd/system/serial-getty@.service"
+    sed -i --follow-symlinks "s/-o '-p -- \\\*\\\u'/--autologin ${FIRST_USER_NAME}/" "${ROOTFS_DIR}/usr/lib/systemd/system/getty@.service"
+    sed -i --follow-symlinks "s/-o '-p -- \\\*\\\u'/--autologin ${FIRST_USER_NAME}/" "${ROOTFS_DIR}/usr/lib/systemd/system/serial-getty@.service"
 fi
