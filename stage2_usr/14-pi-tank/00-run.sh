afea569eefef4506e184ee9b5b6c2fdb2b4c3ada
Stulov Tikhon <kudesnick@inbox.ru>
2024-11-18 20:31:27 +0300

    add ro mode (not debug)

--- stage2_usr/14-pi-tank/00-run.sh
+++ stage2_usr/14-pi-tank/00-run.sh
@@ -2,6 +2,6 @@
 
 install -m 555 files/lcd_hat_keyboard-overlay.dts ${ROOTFS_DIR}
 on_chroot <<- EOF
-    dtc -I dts -O dtb -o "/boot/overlays/lcd_hat_keyboard.dtbo" "lcd_hat_keyboard-overlay.dts" -W no-unit_address_vs_reg
+	dtc -I dts -O dtb -o "/boot/overlays/lcd_hat_keyboard.dtbo" "lcd_hat_keyboard-overlay.dts" -W no-unit_address_vs_reg
 EOF
 rm ${ROOTFS_DIR}/lcd_hat_keyboard-overlay.dts
